6303ad9d9114bdb25dc8dfd868dcbfef90022c2b
Your Name <your_email@example.com>
2024-05-01 18:24:17 +0100

    add data

--- index.js
+++ index.js
@@ -3,8 +3,35 @@ const app=express();
 app.use(express.json());
 
 
+app.get("/data", (req,res) => {
 
+    let number = "";
+    for(let i=0; i<= 100; i++){
+        number +=  i + "-";
+    }
+    //res.status(200).send({number: number})
+    //res.send("<h1>hello</h1>")
+    //res.sendFile( __dirname + "/views/numbers.html")
+    res.render("numbers.ejs", 
+    {
+        id : "5",
+        numb: number,
+    });
+})
 
+app.get("/datanumber/:num1/:num2", (req, res) => {
+    const num1 = req.params.num1
+    const num2 = req.params.num2
+    const total = Number(num1)+Number(num2);
+    res.send(` ${num1} + ${num2} = ${total} `);
+
+})
+
+app.get("/databody", (req, res) => {
+    const data = req.body;
+    res.json(data);
+    // res.send(data);
+})
 
 
 
